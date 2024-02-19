0cf8afe2f029383e3e046e59fd97ab9436fd8547
Art San <artsan.mcc@gmail.com>
2024-02-19 22:09:48 +0700

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// import path from 'path'
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -13,7 +13,7 @@ import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000
 
-// const __dirname = path.resolve() // перед деплоем
+const __dirname = path.resolve() // перед деплоем
 
 dotenv.config()
 
@@ -24,11 +24,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist'))) // middleware перед деплоем
+app.use(express.static(path.join(__dirname, '/frontend/dist'))) // middleware перед деплоем
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html')) // перед деплоем
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html')) // перед деплоем
+})
 
 server.listen(PORT, () => {
   connectToMongoDB()
